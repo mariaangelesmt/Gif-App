513b92d2b8ee9402cacb7e2cd7efe3a74572471c
Maria Angeles <Angy@MacBook-Pro-de-Jose.local>
2021-07-29 12:26:46 +0100

    created GifExpertApp component

---
+++ src/GifExpertApp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GifExpertApp = props => {
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr/>    
+        </>
+    )
+}
+
+GifExpertApp.propTypes = {
+
+}
+
+export default GifExpertApp
